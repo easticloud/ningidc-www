7ef3e44a1da89148c66d07249dc7c9ba9b69440f
Kaviilee <jiaweilee95@126.com>
2025-01-06 16:04:30 +0800

    chore: update meta desc

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
 module.exports = {
     // SEO
     title: "四重奏官网",
-    description: "四重奏官网",
+    description: "四重奏是一家专往于网络安全服务、云计算服务、数据中心租赁托管以及边缘计算等互联网综合业务",
 
     // URL
     __cdn: "https://console.cnyixun.com/easticloud/",
+    __img: "https://img.easticloud.com/",
 
     // API
     API:{
